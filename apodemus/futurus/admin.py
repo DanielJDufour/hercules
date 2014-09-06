ac2ff95272d620089d4e65d226ee6295c0555ad6
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2014-09-06 15:16:23 -0400

    Update admin.py

--- apodemus/futurus/admin.py
+++ apodemus/futurus/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from futurus import Organization
+from futurus import models
 
 class OrganizationAdmin(admin.ModelAdmin):
   prepopulated_fields = {'slug': ('name',)}
 
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(Organization, OrganizationAdmin)
 admin.site.register(Organization, OrganizationAdmin)
