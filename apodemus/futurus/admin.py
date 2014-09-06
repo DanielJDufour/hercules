63cfb08a2ad764cd6acfad690a89ba61c699b0ed
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2014-09-06 15:19:20 -0400

    Update admin.py

--- apodemus/futurus/admin.py
+++ apodemus/futurus/admin.py
@@ -5,6 +5,14 @@ class OrganizationAdmin(admin.ModelAdmin):
   prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Organization, OrganizationAdmin)
-admin.site.register(Organization, OrganizationAdmin)
-admin.site.register(Organization, OrganizationAdmin)
-admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(Membership)
+admin.site.register(Biography)
+admin.site.register(Location)
+admin.site.register(Privacy)
+admin.site.register(Donor)
+admin.site.register(Donation)
+admin.site.register(Project)
+admin.site.register(Link)
+admin.site.register(YouTubeVideo)
+admin.site.register(FacebookPage)
+admin.site.register(Twitter)
